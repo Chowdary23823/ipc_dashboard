db2111e8bd80ad9f12d7d8dad1afc0249b81ab2b
Arumalla Kushal Chowdary <kushalchowdary23@gmail.com>
2025-09-24 01:55:47 +0530

    with sevice account

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ let Tracking_view = null;
 
 // Google Sheets auth
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.join("service-account.json"),
+  keyFile: path.join("./service-account.json"),
   scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
 });
 
